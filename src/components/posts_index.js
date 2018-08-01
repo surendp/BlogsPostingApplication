49d9ea86a4e73e031663b1520c3900d0acb0748e
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-08-01 20:56:51 +0300

    Create posts reducer, posts action creator and render the posts

---
+++ src/components/posts_index.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component{
+
+  componentDidMount(){
+     this.props.fetchPosts();
+  }
+
+  renderPosts(){
+    return(
+          _.map(this.props.posts, post => (
+            <li className = "list-group-item" key={post.id}>
+              {post.title}
+            </li>
+          ))
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ( { posts } ) => (
+  { posts }
+)
+
+ const mapDispatchToProps = ( dispatch ) => (
+   bindActionCreators({ fetchPosts }, dispatch)
+ )
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import  promiseMiddleware from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
+
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component = { PostsIndex }/>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
